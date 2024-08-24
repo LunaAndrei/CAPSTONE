6112fa9261e28721548c3a4fac127fca458eb927
JohnCarloLagunsing <jc6690304@gmail.com>
2024-08-24 22:04:50 +0800

    updating the id in OccuPermit and update icon

--- ServerFunc/Occuprocess.js
+++ ServerFunc/Occuprocess.js
@@ -33,8 +33,8 @@ app.post('/submitForm', upload.fields([
 ]), async (req, res) => {
     const {
         lastName, firstName, middleName, suffix, homeAddress, dob, age, placeOfBirth,
-        cellphoneNumber, email, gender, civilStatus, companyName, jobPosition,Typeofid,
-        philSysId, orNumber, orExtension, orDate, orAmount,ctcNumber,ctcDateIssued,ctcPlaceIssued
+        cellphoneNumber, email, gender, civilStatus, companyName, jobPosition, combinedId,
+        orNumber, orExtension, orDate, orAmount, ctcNumber, ctcDateIssued, ctcPlaceIssued
     } = req.body;
 
     // Get file paths from multer
@@ -46,21 +46,23 @@ app.post('/submitForm', upload.fields([
     try {
         const query = `
             INSERT INTO public."OccuPermit" (
-                "Occuid", "Lastname", "Firstname", "Middlename", "Suffix", "Address", "DateofBirth", "Age", "PlaceofBirth",
-                "ContactNo", "Email", "Gender", "CivilStatus", "CompanyName", "JobPosition", "Typeofid",
-                "PhilSysId", "COE", "HealthCard", "BirthCertificate", "OfficialReceipt",
-                "ORNumber", "ORExtension", "ORDate", "ORAmount","CTCNumber", "CTCDateIssued", "CTCPlaceIssued"
+                "Lastname", "Firstname", "Middlename", "Suffix", "Address", "DateofBirth", "Age", "PlaceofBirth",
+                "ContactNo", "Email", "Gender", "CivilStatus", "CompanyName", "JobPosition", "combinedId",
+                "COE", "HealthCard", "BirthCertificate", "OfficialReceipt",
+                "ORNumber", "ORExtension", "ORDate", "ORAmount", "CTCNumber", "CTCDateIssued", "CTCPlaceIssued"
             ) VALUES (
-                DEFAULT, $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12,
-                $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23,$24,$25,$26,$27
+                $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12,
+                $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26
             )
         `;
-        const ctcDateIssued = new Date(req.body.ctcDateIssued).toISOString().split('T')[0];
+        
+        const formattedCtcDateIssued = new Date(ctcDateIssued).toISOString().split('T')[0];
+
         const values = [
             lastName, firstName, middleName, suffix, homeAddress, dob, age, placeOfBirth,
-            cellphoneNumber, email, gender, civilStatus, companyName, jobPosition,
-            philSysId, coe, healthCard, birthCertificate, officialReceipt,Typeofid,
-            orNumber, orExtension, orDate, orAmount,ctcNumber,ctcDateIssued,ctcPlaceIssued
+            cellphoneNumber, email, gender, civilStatus, companyName, jobPosition, combinedId,
+            coe, healthCard, birthCertificate, officialReceipt,
+            orNumber, orExtension, orDate, orAmount, ctcNumber, formattedCtcDateIssued, ctcPlaceIssued
         ];
 
         await pool.query(query, values);
@@ -70,6 +72,7 @@ app.post('/submitForm', upload.fields([
         res.status(500).send('Error submitting form');
     }
 });
+
 
 // Serve static files from 'uploads' directory
 app.use('/uploads', express.static('uploads'));
